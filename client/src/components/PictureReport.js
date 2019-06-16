a1601bf8bdd5443fc92004eb892ded1d57cc7828
Tony Choi <tchoi52@gmail.com>
2019-06-16 19:32:42 -0400

    dynamic pic arrangement

--- client/src/components/PictureReport.js
+++ client/src/components/PictureReport.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Row, Col, Button } from "react-bootstrap";
+import { Grid, Row, Col } from "react-bootstrap";
 import "./PictureReport.css";
 
 class pictureReport extends Component {
@@ -11,6 +11,7 @@ class pictureReport extends Component {
             }
         }
         catch(e){
+          super(props);
             this.state = {
                 cameras: null
             }
@@ -25,7 +26,10 @@ class pictureReport extends Component {
             </tr>
           );
         else {
-          const results = this.state.cameras.map(camera => (
+          const results = this.state.cameras.map(function(camera) {
+            let num = camera.headNum;
+            if(num === 1) {
+              return(
             <Grid className="pictureGrid"key={camera._id}>
               <Row className="pictureInfos">
               <Col xs={3} className="pInfos">
@@ -34,18 +38,54 @@ class pictureReport extends Component {
                 <b>Captured On: </b>{new Date(camera.date).toLocaleDateString("en-US")}
               </Col>
                 
-
               <Col xs={9}>
-                <img src={camera.picDetails[0]} width={270} height={210} alt="Image Not Available" />
-                <img src={camera.picDetails[1]} width={270} height={210} />
-                <img src={camera.picDetails[2]} width={270} height={210} />
-                <img src={camera.picDetails[3]} width={270} height={210} />
+                <img src={camera.picDetails[0]} width={270} height={210} alt="Not Available"/>
+
+              </Col>
+              </Row>
+            </Grid>)
+            } else if(num === 3) {
+              return(
+              <Grid className="pictureGrid"key={camera._id}>
+              <Row className="pictureInfos">
+              <Col xs={3} className="pInfos">
+              <div className="deviceNameInfo"><b>Device Name: </b> {camera.deviceName} <br></br></div>
+                <b>IP Address: </b>{camera.ip} <br></br>
+                <b>Captured On: </b>{new Date(camera.date).toLocaleDateString("en-US")}
+              </Col>
+                
+              <Col xs={9}>
+                <img src={camera.picDetails[0]} width={270} height={210} alt="Not Available"/>
+                <img src={camera.picDetails[1]} width={270} height={210} alt="Not Available"/>
+                <img src={camera.picDetails[2]} width={270} height={210} alt="Not Available"/>
 
               </Col>
               </Row>
             </Grid>
+              )
+            } else {
+              return(
+              <Grid className="pictureGrid"key={camera._id}>
+              <Row className="pictureInfos">
+              <Col xs={3} className="pInfos">
+              <div className="deviceNameInfo"><b>Device Name: </b> {camera.deviceName} <br></br></div>
+                <b>IP Address: </b>{camera.ip} <br></br>
+                <b>Captured On: </b>{new Date(camera.date).toLocaleDateString("en-US")}
+              </Col>
+                
+              <Col xs={9}>
+                <img src={camera.picDetails[0]} width={270} height={210} alt="Not Available" />
+                <img src={camera.picDetails[1]} width={270} height={210} alt="Not Available"/>
+                <img src={camera.picDetails[2]} width={270} height={210} alt="Not Available"/>
+                <img src={camera.picDetails[3]} width={270} height={210} alt="Not Available"/>
+
+              </Col>
+              </Row>
+            </Grid>
+              )
+            }
             
-          ));
+          });
           return results;
         }
       }
